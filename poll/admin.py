e59cdc9bd270cc97611baba884e80bc7094173f7
ECHO <chigozie10@gmail.com>
2022-09-19 09:56:24 +0100

    Remove choice admin and use choice inline

--- poll/admin.py
+++ poll/admin.py
@@ -4,6 +4,12 @@ from core.admin import admin_site
 from poll.models import Choice, Question
 
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    fields = ("choice_text", "votes")
+    extra = 1
+
+
 @admin.register(Question, site=admin_site)
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ("question_text", "status")
@@ -13,33 +19,11 @@ class QuestionAdmin(admin.ModelAdmin):
         "created_by",
         "modified_by",
     )
+    inlines = [ChoiceInline]
 
     def save_model(self, request, obj, form, change):
         if not obj.created_by:
             obj.created_by = request.user
-
-        obj.modified_by = request.user
-        return super().save_model(request, obj, form, change)
-
-
-@admin.register(Choice, site=admin_site)
-class ChoiceAdmin(admin.ModelAdmin):
-    list_display = ("choice_text", "votes")
-    readonly_fields = (
-        "created_at",
-        "modified_at",
-        "created_by",
-        "modified_by",
-        "question",
-    )
-
-    def save_model(self, request, obj, form, change):
-        if not obj.created_by:
-            obj.created_by = request.user
-
-        # We may change this in future to allow for admin to select question
-        if not obj.question_id:
-            obj.question = Question.objects.get(status=Question.ACTIVE)
 
         obj.modified_by = request.user
         return super().save_model(request, obj, form, change)
